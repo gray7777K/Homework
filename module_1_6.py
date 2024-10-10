51ece2b7fe9113d31eb00ff4d6e69b7fb2b3aabb
gray7777K <keldyshp@gmail.com>
2024-10-10 05:27:23 +0300

    Add files via upload
    Практическое задание по теме "Словари и множества"

---
+++ module_1_6.py
@@ -0,0 +1,15 @@
+#my_dict = {'Миша': 1965, 'Рома': 1984, "Карл": 1913, 'Ольга': 2005}
+#print(my_dict)
+#print(my_dict['Рома'])
+#print(my_dict.get('Владимир'))
+#my_dict.update({"Георгий": 2011, "Владислав": 1945})
+#print(my_dict)
+#izyatie = my_dict.pop('Рома')
+#print(izyatie)
+#print(my_dict)
+
+my_set = {45, "море", 87, 1, 87, 'Николай', "море", 45, 99, 3}
+print(my_set)
+my_set.update([85, "Гриша"])
+my_set.remove(45)
+print(my_set)
