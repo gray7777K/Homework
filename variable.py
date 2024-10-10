921ef5d0583d69fcc5ab5c54c3cd861d4048a6eb
gray7777K <keldyshp@gmail.com>
2024-10-10 05:15:19 +0300

    Add files via upload
    Практическое задание по теме "Переменные"

---
+++ variable.py
@@ -0,0 +1,7 @@
+The_number_of_completed_HT = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_for_one_task = Number_of_hours_spent / The_number_of_completed_HT
+print(Time_for_one_task)
+print(str(Time_for_one_task))
+print('Курс: ' + Course_name + ', всего задач:' + str(The_number_of_completed_HT) + ', затрачено часов: ' + str(Number_of_hours_spent) + ', среднее время выполнения ' + str(Time_for_one_task))
