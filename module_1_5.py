c52fa875cd18de222d8c5ca85e3801a074998482
gray7777K <keldyshp@gmail.com>
2024-10-10 05:25:14 +0300

    Add files via upload
    Практическое задание по теме "Неизменяемые и изменяемые объекты. Кортежи"

---
+++ module_1_5.py
@@ -0,0 +1,15 @@
+#immutable_var = (7, "boleslav", False)
+#print(immutable_var)
+#immutable_var[0] = 9
+#immutable_var[1] = True
+#immutable_var[2] = "kirieshka"
+
+#immutable_var_2 = 9, [True, 77, "bublik"], "luna", False
+#immutable_var_2[1][2] = "peregrin"
+#print(immutable_var_2)
+
+mutable_list = ["alex", False, 999]
+mutable_list[0] = True
+mutable_list[1] = "cat"
+mutable_list[2] = 85
+print(mutable_list)
