d4e567dca1bd86f817ad647008221666e972d5fc
gray7777K <keldyshp@gmail.com>
2024-12-02 02:31:32 +0300

    Add files via upload
    Домашнее задание по теме "Сложные моменты и исключения в стеке вызовов функции"

---
+++ module_8_2.py
@@ -0,0 +1,25 @@
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for number in numbers:
+        try:
+            result += number
+        except TypeError:
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчёта суммы - {number}')
+    return result, incorrect_data
+
+def calculate_average(numbers):
+    try:
+        psn = personal_sum(numbers)
+        average = psn[0] / (len(numbers) - psn[1])
+        return average
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print('В numbers записан некорректный тип данных')
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
